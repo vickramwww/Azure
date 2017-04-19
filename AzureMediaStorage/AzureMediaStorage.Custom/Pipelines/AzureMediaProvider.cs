04f9df1488116ff03f0c670942ce9b5cdfa8f1dd
vickram.rameshbabu <vickramwww@gmail.com>
2017-04-19 17:37:17 +0530

    commit azure media storage piepeline

---
+++ AzureMediaStorage/AzureMediaStorage.Custom/Pipelines/AzureMediaProvider.cs
@@ -0,0 +1,83 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Events.Hooks;
+using Sitecore.Resources.Media;
+
+namespace AzureMediaStorage.Custom.Pipelines
+{
+    public class AzureMediaProvider: MediaProvider, IHook
+    {
+        public void Initialize()
+        {
+            MediaManager.Provider = this;
+        }
+
+        public override string GetMediaUrl(MediaItem item)
+        {
+            string mediaUrl = base.GetMediaUrl(item);
+            return mediaUrl;
+        }
+
+        public override string GetMediaUrl(MediaItem item, MediaUrlOptions options)
+        {
+            string mediaUrl = base.GetMediaUrl(item, options);
+            return GetCstmMediaUrl(mediaUrl, item);
+        }
+
+        /// <summary>
+        /// Determines if media should be pulling from the CDN or not
+        /// </summary>
+        /// <param name="mediaUrl"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>               
+        public string GetCstmMediaUrl(string mediaUrl, MediaItem item)
+        {
+            //verify the domain was set in the config
+            if (string.IsNullOrEmpty(OriginPrefix))
+            {
+                return mediaUrl;
+            }
+
+            //Condition to fetch the Azure url only if the site is rendered and not for Preview or editing mode.            
+            if (Sitecore.Context.GetSiteName().ToLower() != "website" || Sitecore.Context.PageMode.IsExperienceEditor || Sitecore.Context.PageMode.IsPreview || Sitecore.Context.PageMode.IsSimulatedDevicePreviewing)
+            {
+                return mediaUrl;
+            }
+            mediaUrl = mediaUrl.Replace("~/media/", "/");
+            //this happens while indexing unless the proper site is set
+            mediaUrl = mediaUrl.Replace("/sitecore/shell/", "/");
+            mediaUrl = mediaUrl.Replace("//", "/");
+            //reference the file in the cdn by the actual extension
+            mediaUrl = mediaUrl.Replace(".ashx", "." + item.Extension);
+            mediaUrl = string.Format("{0}{1}", OriginPrefix, mediaUrl);
+            string Language = string.Empty;
+            if (mediaUrl.Contains("la="))
+            {
+                Language = mediaUrl.Substring(mediaUrl.IndexOf("la=") + 3);
+                if (Language.Contains("&"))
+                    Language = Language.Substring(0, Language.IndexOf("&"));
+            }
+            else
+            {
+                Language = Sitecore.Configuration.Settings.DefaultLanguage;
+            }
+            //create the media url accoriding to the naming convention of Azure-media-file name
+            mediaUrl = mediaUrl.Replace(item.DisplayName + "." + item.Extension, item.ID.ToString().Replace("{", "").Replace("}", "").Replace("-", "") + "-" + Language + "." + item.Extension);
+            //if (HttpContext.Current != null && HttpContext.Current.Request.IsSecureConnection)
+            //{
+            //    //if we are on a secure connection, make sure we are making an https url over to the cdn
+            //    mediaUrl = mediaUrl.Replace("http://", "https://");
+            //}
+            return mediaUrl;
+        }
+
+
+        public string OriginPrefix
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.AppSettings["OrignalPrefix"];
+
+            }
+        }
+    }
+}
---
+++ AzureMediaStorage/AzureMediaStorage.Custom/Pipelines/AzureStorage.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureMediaStorage.Custom.Pipelines
+{
+    public class AzureStorage
+    {
+        CloudStorageAccount storageAccount;
+        CloudBlobClient blobClient;
+        CloudBlobContainer container;
+
+        public AzureStorage()
+        {
+            storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"].ToString());
+            blobClient = storageAccount.CreateCloudBlobClient();
+            container = blobClient.GetContainerReference(ConfigurationManager.AppSettings["AzureContainer"]);
+            container.CreateIfNotExists();
+
+            container.SetPermissions(
+                new BlobContainerPermissions
+                {
+                    PublicAccess = BlobContainerPublicAccessType.Blob
+                });
+        }
+
+        public void UploadMediaToAzure(MediaItem mediaItem, string extension = "", string language = "" )
+        {
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(mediaItem.MediaPath.TrimStart('/').Replace(mediaItem.DisplayName, mediaItem.ID.ToString().Replace("{", "").Replace("}", "").Replace("-", "")) + "-" + language + "." + extension);
+            blockBlob.DeleteIfExists();
+
+            if (string.IsNullOrEmpty(mediaItem.Extension))
+                return;
+            if(mediaItem.HasMediaStream("Media"))
+            {
+                using (var fs = (FileStream)mediaItem.GetMediaStream())
+                {
+                    blockBlob.UploadFromStream(fs);
+                }
+            }
+            else
+            {
+                blockBlob.DeleteIfExists();
+            }
+        }
+
+        public void DeleteMediaFromAzure(MediaItem mediaItem, string extension = "", string language = "")
+        {
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(mediaItem.MediaPath.TrimStart('/').Replace(mediaItem.DisplayName, mediaItem.ID.ToString().Replace("{", "").Replace("}", "").Replace("-", "")) + "-" + language + "." + extension);
+
+            blockBlob.DeleteIfExists();
+        }
+
+        public void ReplaceMediaFromAzure(MediaItem mediaItem, string extension="", string language="")
+        {
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(mediaItem.MediaPath.TrimStart('/').Replace(mediaItem.DisplayName, mediaItem.ID.ToString().Replace("{", "").Replace("}", "").Replace("-", "")) + "-" + language + "." + extension);
+
+            blockBlob.DeleteIfExists();
+
+            if (string.IsNullOrEmpty(mediaItem.Extension))
+                return;
+            using (var fs = mediaItem.GetMediaStream())
+            {
+                blockBlob.UploadFromStream(fs);
+            }
+
+        }
+
+
+
+    }
+}
